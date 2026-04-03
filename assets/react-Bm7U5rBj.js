e53c39f77f62b1a576bdd7d2ab2b09cb35d7d73d
Mogikan <Mogikan@users.noreply.github.com>
2026-04-03 08:03:47 +0000

    deploy: 829624c36c11aa1f8f4b35428ec182194125fb36

--- assets/react-Bm7U5rBj.js
+++ assets/react-Bm7U5rBj.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-Dp-mANGI.js";import{M as P,C as A}from"./mixin-BY5xW4uI.js";import{H as h}from"./hls-CplUDmW5.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-BZRzQvM_.js";import{M as P,C as A}from"./mixin-BY5xW4uI.js";import{H as h}from"./hls-CplUDmW5.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
